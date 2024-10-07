d47cdb32c42b3300bf2d667e89e9b23aec261ae1
Jannis Iking <jannis.iking@gmail.com>
2024-10-07 16:08:02 +0200

    day4 parse cards

--- src/main/java/org/example/day4/Card.java
+++ src/main/java/org/example/day4/Card.java
@@ -1,11 +1,30 @@
 package org.example.day4;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Arrays;
 import java.util.List;
 
+@Getter
+@Builder
 public class Card {
 
-    String cardId;
-    List<String> winningNumbers;
-    List<String> numbersYouHave;
+    private Integer cardId;
+    private List<Integer> winningNumbers;
+    private List<Integer> numbersYouHave;
 
+    public static Card createCardFromInput(String input){
+        Integer cardId = Integer.valueOf(input.split(":")[0].replace("Card", "").trim());
+
+        List<Integer> winningNumbers = Arrays.stream(input.split(":")[1].split("\\|")[0].split(" ")).filter(possibleEmpty -> !possibleEmpty.isEmpty()).map(Integer::valueOf).toList();
+        List<Integer> numbersYouHave = Arrays.stream(input.split(":")[1].split("\\|")[1].split(" ")).filter(possibleEmpty -> !possibleEmpty.isEmpty()).map(Integer::valueOf).toList();
+
+        return Card.builder()
+                .cardId(cardId)
+                .winningNumbers(winningNumbers)
+                .numbersYouHave(numbersYouHave)
+                .build();
+
+    }
 }
--- src/test/java/org/example/day3/Day4Test.java
+++ src/test/java/org/example/day3/Day4Test.java
@@ -1,22 +1,22 @@
 package org.example.day3;
 
-import org.example.day4.Day4;
+import org.example.day4.Card;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day4Test {
 
     @Test
-    void testPart1WithSampleData(){
-        String result = Day4.day4(1, "src/test/resources/day4.txt");
-        assertThat(result).isEqualTo("4361");
-    }
+    public void canParseInputStringToCard(){
+        String inputString = "Card   1: 69 72 87 33 61 15  8 78 43 50 | 96 33 86 53 15 82 50 85 61  8 98 72 43 63 45 78 87 69 10 34 73 88 65 27 19";
+        Card card = Card.createCardFromInput(inputString);
 
-    @Test
-    void testPart2WithSampleData(){
-        String result = Day4.day4(2, "src/test/resources/day4.txt");
-        assertThat(result).isEqualTo("467835");
+        assertThat(card.getCardId()).isEqualTo(1);
+        assertThat(card.getWinningNumbers()).isEqualTo(List.of(69, 72, 87, 33, 61, 15, 8, 78, 43, 50));
+        assertThat(card.getNumbersYouHave()).isEqualTo(List.of(96, 33, 86, 53, 15, 82, 50, 85, 61, 8, 98, 72, 43, 63, 45, 78, 87, 69, 10, 34, 73, 88, 65, 27, 19));
     }
 
 }
